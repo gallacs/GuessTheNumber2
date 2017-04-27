cec0f164d6b19b09f8346bf1391a2f33c1f09b0f
gallacs <gallacs@gmail.com>
2017-04-27 17:33:09 +0200

    Timer is working

--- app/src/main/java/com/example/gallacs/guessthenumber/Game.java
+++ app/src/main/java/com/example/gallacs/guessthenumber/Game.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -33,12 +34,32 @@ public class Game extends Activity {
     int allScore = 0;
     public static String scoreName="";
     public static SQLiteDatabase database;
+    CountDownTimer timer;
 
     Button guessButton, endButton;
-    TextView textYourScore, scoreText, textYourTries, triesText;
+    TextView textYourScore, scoreText, textYourTries, triesText, timeText;
     NumberPicker first, second, third;
     int guess1, guess2, guess3;
     int[] randomNumbers = new int[3];
+
+//    public void createDatabase() {
+//
+//        try{
+//            database = this.openOrCreateDatabase("Scores", MODE_PRIVATE, null);
+//            database.execSQL("CREATE TABLE IF NOT EXISTS Scores " +
+//                    "(name VARCHAR, score INTEGER);");
+//            database.execSQL("CREATE TABLE IF NOT EXISTS Tries " +
+//                    "( firstG INTEGER, secondG INTEGER, thirdG INTEGER, match INTEGER, goodPlaceToo INTEGER );");
+//            File database = getApplicationContext().getDatabasePath("Scores");
+//            if (database.exists()) {
+//                Toast.makeText(this, "Database Created", Toast.LENGTH_SHORT).show();
+//            } else {
+//                Toast.makeText(this, "Database Missing", Toast.LENGTH_SHORT).show();
+//            }
+//        } catch(Exception e){
+//            Log.e("CONTACTS ERROR", "Error Creating Database");
+//        }
+//    }
 
     public void createDatabase() {
 
@@ -57,11 +78,57 @@ public class Game extends Activity {
         }
     }
 
-    public void deleteDatabase(View view){
-        // Delete database
-        this.deleteDatabase("Scores");
-
+    public void addContact(String name, int scoreDB) {
+        // Execute SQL statement to insert new data
+        database.execSQL("INSERT INTO Scores (name, score) VALUES ('" + name + "', " + scoreDB + ");");
     }
+
+//    public void addTries(int first, int second, int third, int matches, int goodplacetoo) {
+//        // Execute SQL statement to insert new data
+//        database.execSQL("INSERT INTO Scores (name, score) VALUES (" + first + ", " + second + ", " + third + ", " + matches + ", " + goodplacetoo + ");");
+//    }
+
+//    public void getTries() {
+//
+//        Cursor cursor = database.rawQuery("SELECT * FROM Tries", null);
+//
+//        int firstColumn = cursor.getColumnIndex("firstG");
+//        int secondColumn = cursor.getColumnIndex("secondG");
+//        int thirdColumn = cursor.getColumnIndex("thirdG");
+//
+//        int matchColumn = cursor.getColumnIndex("match");
+//        int goodPlaceTooColumn = cursor.getColumnIndex("goodPlaceToo");
+//
+//        cursor.moveToFirst();
+//        String triesList = "";
+//
+//        if(cursor != null && (cursor.getCount() > 0)){
+//            do{
+//                // Get the results and store them in an int
+//                int first = cursor.getInt(firstColumn);
+//                int second = cursor.getInt(secondColumn);
+//                int third = cursor.getInt(thirdColumn);
+//                int match = cursor.getInt(matchColumn);
+//                int goodPlaceToo = cursor.getInt(goodPlaceTooColumn);
+//
+//                triesList = triesList + first + second + third + " : " + match + ", " + goodPlaceToo + "\n";
+//                // Keep getting results as long as they exist
+//            }while(cursor.moveToNext());
+//            guessesText.setText(triesList);
+//        } else {
+//            Toast.makeText(this, "No Results to Show", Toast.LENGTH_SHORT).show();
+//            guessesText.setText("");
+//        }
+//    }
+
+    public void deleteDatabase(View view){
+        this.deleteDatabase("Scores");
+        Toast.makeText(this, "The database was deleted", Toast.LENGTH_SHORT).show();
+    }
+
+//    public void dropTriesTable(){
+//        database.execSQL("DROP TABLE IF EXISTS Tries");
+//    }
 
     public int[] randomMaker(){
         int[] randomnums = new int[3];
@@ -86,6 +153,20 @@ public class Game extends Activity {
         return message;
     }
 
+    public void timerStart(){
+        timeText.setText("60");
+        timer : new CountDownTimer(60 * 1000, 1000){
+            @Override
+            public void onTick(long millisecund){
+                timeText.setText("" + millisecund / 1000);
+            }
+            @Override
+            public void onFinish(){
+                timeText.setText("Time is up!");
+            }
+        }.start();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -95,6 +176,7 @@ public class Game extends Activity {
         scoreText = (TextView) findViewById(R.id.scoreText);
         textYourTries = (TextView) findViewById(R.id.textYourTries);
         triesText = (TextView) findViewById(R.id.triesText);
+        timeText = (TextView) findViewById(R.id.timeText);
 
         guessButton = (Button) findViewById(R.id.guessButton);
         endButton = (Button) findViewById(R.id.endButton);
@@ -108,18 +190,14 @@ public class Game extends Activity {
             third.setMaxValue(9);
             third.setMinValue(1);
         randomNumbers = randomMaker();
+        timerStart();
 
         //Toast.makeText(this,randomNumbers[0] + " " + randomNumbers[1] + " " + randomNumbers[2], Toast.LENGTH_LONG).show();
     }
 
-    public void addContact(String name, int scoreDB) {
-
-        // Execute SQL statement to insert new data
-        database.execSQL("INSERT INTO Scores (name, score) VALUES ('" + name + "', " + scoreDB + ");");
-
-    }
-
     public void onButtonClick(View view){
+        String remainingTime = timeText.getText().toString();
+
 
         //deleteDatabase(view);
         int[] guessEs = new int[3];
@@ -129,6 +207,9 @@ public class Game extends Activity {
         int match = 0;
         int goodPlaceToo = 0;
         boolean game = true;
+
+        //createDatabase();
+        //addTries(guessEs[0], guessEs[1], guessEs[2], match, goodPlaceToo);
 
         if (isThereDouble(guessEs) != null) {
             Toast.makeText(this, isThereDouble(guessEs), Toast.LENGTH_SHORT).show();
@@ -150,9 +231,10 @@ public class Game extends Activity {
             //Toast.makeText(this, "Tries: " + tries, Toast.LENGTH_SHORT).show();
             Toast.makeText(this, "Found numbers: " + match + " In place: " + goodPlaceToo, Toast.LENGTH_SHORT).show();
 
-            if(tries == 10){
+            if(tries == 10 || remainingTime == "Time is up!"){
                 AlertDialog.Builder dlgAlert = new AlertDialog.Builder(this);
                 dlgAlert.setMessage("You can not find the solution." +
+                        "\nThe solution: " + randomNumbers[0] + randomNumbers[1] + randomNumbers[2] +
                         "\nTries number: " + tries +
                         "\nYour score for this turn: "  + score +
                         "\nAll Score: " + allScore +
@@ -169,6 +251,7 @@ public class Game extends Activity {
 
                 AlertDialog.Builder dlgAlert = new AlertDialog.Builder(this);
                 dlgAlert.setMessage("You were great! You found every numbers." +
+                        "\nThe solution: " + randomNumbers[0] + randomNumbers[1] + randomNumbers[2] +
                         "\nTries number: " + tries +
                         "\nYour score for this turn: "  + score +
                         "\nAll Score: " + allScore +
@@ -187,6 +270,7 @@ public class Game extends Activity {
             triesText.setText(String.valueOf(tries));
             game = false;
         }
+        //getTries();     //Get the tries values
     }
 
 //    public void countdown(View view){
@@ -202,6 +286,7 @@ public class Game extends Activity {
 //    }
 
     public void onEndClick(View view){
+        //deleteDatabase(view);
         createDatabase();
 //        countdown();
         if (view == null || view.getId() == R.id.endButton) {
@@ -230,5 +315,6 @@ public class Game extends Activity {
             });
             builder.show();
         }
+        //dropTriesTable();
     }
 }
